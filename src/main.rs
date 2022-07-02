37cf142655354d0ec04190f26442573ec902eeb6
quantumish <freifeld.david@gmail.com>
2022-07-02 20:29:39 +0000

    Flush for improved speed.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 #![feature(never_type)]
 use std::io::{self, Write, Error};
-//const BUFSIZ: usize = 8192*7;
-const BUFSIZ: usize = 8192*1;
+const BUFSIZ: usize = 8192*7;
 
 fn main() -> Result<!, Error> {
     let buf = "n\n".repeat(BUFSIZ/2).into_bytes();
-    let mut stdout = io::stdout().lock();
-    //let mut out = io::stdout().lock();
-    let mut out = io::BufWriter::new(stdout);
-    loop {out.write(&buf)?;};
+	let mut stdout = io::stdout().lock();
+    loop {stdout.write(&buf)?; stdout.flush();};
 }
