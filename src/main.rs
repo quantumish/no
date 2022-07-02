27f72114e1c09e337a0aef1f679d7ce9873f23d2
quantumish <freifeld.david@gmail.com>
2022-07-02 04:38:20 +0000

    Small cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
+#![feature(never_type)]
 use std::io::{self, Write, Error};
 const BUFSIZ: usize = 8192*7;
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<!, Error> {
     let mut buf: [u8; BUFSIZ] = [110; BUFSIZ];
-    for i in 0..BUFSIZ/2 {
-        buf[i*2] = 10;
-    }
+    for i in 0..BUFSIZ/2 {buf[i*2] = 10;}
     loop {io::stdout().write(&buf)?;};
-    Ok(())
 }
