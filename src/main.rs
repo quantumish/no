15798add0a4dfa06c326aeca3b04e6c14f826f3f
quantumish <freifeld.david@gmail.com>
2022-07-02 04:50:41 +0000

    Switch to using .repeat()

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use std::io::{self, Write, Error};
 const BUFSIZ: usize = 8192*7;
 
 fn main() -> Result<!, Error> {
-    let mut buf: [u8; BUFSIZ] = [110; BUFSIZ];
-    for i in 0..BUFSIZ/2 {buf[i*2] = 10;}
+    let buf = "n\n".repeat(BUFSIZ/2).into_bytes();
     loop {io::stdout().write(&buf)?;};
 }
