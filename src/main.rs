b96a331c410321db9123dcda81fc356c49844cfc
quantumish <freifeld.david@gmail.com>
2022-07-02 21:05:14 +0000

    Switch to

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![feature(never_type)]
-use std::io::{self, Write, Error};
-const BUFSIZ: usize = 8192*7;
+use std::io::{self, IoSlice};
+use std::os::unix::io::{AsRawFd};
+use nix::fcntl::{vmsplice, SpliceFFlags};
+const BUFSIZ: usize = 8192*4;
 
-fn main() -> Result<!, Error> {
+fn main() -> ! {
     let buf = "n\n".repeat(BUFSIZ/2).into_bytes();
-	let mut stdout = io::stdout().lock();
-    loop {stdout.write(&buf)?; stdout.flush();};
+    let stdout = io::stdout().lock().as_raw_fd();
+    loop {vmsplice(stdout, &[IoSlice::new(&buf)], SpliceFFlags::SPLICE_F_NONBLOCK);}
 }
