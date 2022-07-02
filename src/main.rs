872b9a8bf400c1d3fb78503633e39a75994e68a3
Huxley Marvit <huxmarv@nuevaschool.org>
2022-07-02 20:54:32 +0000

    separate lines and .unwrap() for marginal speed improvements ~+.3gbs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![feature(never_type)]
 use std::io::{self, Write, Error};
-const BUFSIZ: usize = 8192*7;
+const BUFSIZ: usize = 8192*1;
 
 fn main() -> Result<!, Error> {
     let buf = "n\n".repeat(BUFSIZ/2).into_bytes();
-	let mut stdout = io::stdout().lock();
-    loop {stdout.write(&buf)?; stdout.flush();};
+    let out = io::stdout();
+    let mut stdout = out.lock();
+    loop {stdout.write(&buf).unwrap(); stdout.flush();};
 }
