61a13114f5a5be04e4ddf97a188e2cdfbf15c4de
quantumish <freifeld.david@gmail.com>
2022-07-02 04:27:59 +0000

    Remove tabs.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use std::io::{self, Write, Error};
 const BUFSIZ: usize = 8192*7;
 
 fn main() -> Result<(), Error> {
-	let mut buf: [u8; BUFSIZ] = [110; BUFSIZ];
-	for i in 0..BUFSIZ/2 {
-		buf[i*2] = 10;
-	}
-	loop {io::stdout().write(&buf)?;};
-	Ok(())
+    let mut buf: [u8; BUFSIZ] = [110; BUFSIZ];
+    for i in 0..BUFSIZ/2 {
+        buf[i*2] = 10;
+    }
+    loop {io::stdout().write(&buf)?;};
+    Ok(())
 }
