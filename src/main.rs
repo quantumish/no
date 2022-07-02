8867f963b170f5128b83fb3c7cdaa549c64a9ace
Huxley Marvit <huxmarv@nuevaschool.org>
2022-07-02 20:16:12 +0000

    change buff size to be optimal on arch linux, and add a bufwriter optimization

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #![feature(never_type)]
 use std::io::{self, Write, Error};
-const BUFSIZ: usize = 8192*7;
+//const BUFSIZ: usize = 8192*7;
+const BUFSIZ: usize = 8192*1;
 
 fn main() -> Result<!, Error> {
     let buf = "n\n".repeat(BUFSIZ/2).into_bytes();
-    loop {io::stdout().write(&buf)?;};
+    let mut stdout = io::stdout().lock();
+    //let mut out = io::stdout().lock();
+    let mut out = io::BufWriter::new(stdout);
+    loop {out.write(&buf)?;};
 }
